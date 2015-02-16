013b07413f4eac56d79c4802e790eb8f7918fec7
icersummer <guofangsky@gmail.com>
2015-02-16 14:56:15 +0800

    temp create repository in out of FirstCodeAndroid, since it is not push-able

---
+++ src/com/vjia/bookcollector/MainActivity.java
@@ -0,0 +1,53 @@
+package com.vjia.bookcollector;
+
+
+import com.vjia.bookcollector.isbn.IsbnFileUtils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	private final static String CLASSNAME = MainActivity.class.getName();
+	private final static int SCANNIN_GREQUEST_CODE = 1;
+	/**
+	 * 显示扫描结果
+	 */
+	private TextView mTextView ;
+	/**
+	 * 显示扫描拍的图片
+	 */
+	private ImageView mImageView;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		//点击按钮跳转到二维码扫描界面
+		Button mButton = (Button) findViewById(R.id.main2_start_scan);
+		mButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, MipcaActivityCapture.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+			}
+		});
+		
+
+	}
+	
+
+}
